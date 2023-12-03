dbd323f1de2adce9796e5af2c8460fec666ffd31
Michael Park <gpark608@gmail.com>
2023-12-03 13:17:42 -0700

    Day 1 Completed

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,6 +1,18 @@
 import re
 
 qOneAnswer = 0
+qTwoAnswer = 0
+stringToInt = {
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9'
+}
 def findDigit(line):
     digits=re.findall(r'\d', line)
     firstDigit = digits[0]
@@ -9,14 +21,25 @@ def findDigit(line):
     return int(answer)
 
 def includeStrings(line):
+    digits=re.findall(r'(?=(one|two|three|four|five|six|seven|eight|nine|ten|\d))', line)
+    firstDigit = digits[0]
+    lastDigit = digits[-1]
     
-    return 0
+    if firstDigit in stringToInt:
+        firstDigit = stringToInt[firstDigit]
+    if lastDigit in stringToInt:
+        lastDigit = stringToInt[lastDigit]
+    answer = firstDigit+lastDigit
+    return int(answer)
+
 with open('input.txt') as f:
     while True:
         line = f.readline()
         if not line:
             break
         qOneAnswer += findDigit(line)
+        qTwoAnswer +=includeStrings(line)
     print(qOneAnswer)
+    print(qTwoAnswer)
 
 
